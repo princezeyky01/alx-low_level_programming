f875629c885f80d83acdf3d2baae7f1f901de5b7
Prince Udeh <arinzechukwup@yahoo.com>
2022-09-09 17:59:53 -0700

    A program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+				putchar(ch);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
