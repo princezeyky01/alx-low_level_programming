4e8c4b1f4adf86a65e116e909c61e41ff62e1155
Prince Udeh <arinzechukwup@yahoo.com>
2022-09-10 01:02:00 -0700

    A program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 			for (j = 10; j <= 19; j++)
 		{
 				if ((j % 10) > (i % 10))
-			{																	
+			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
 					if (i != 18 || j != 19)
