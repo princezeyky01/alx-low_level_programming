b4306ac17da57039e7dfbd0cbc959e84b83bc559
Prince Udeh <arinzechukwup@yahoo.com>
2022-09-13 07:16:05 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,12 +7,10 @@
  */
 int main(void)
 {
-
 	int i;
 
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-
 	/*
 	 * while (i < 10)
 	 *
@@ -20,7 +18,6 @@ int main(void)
 	 * putchar(i);
 	 * }
 	 */
-
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
