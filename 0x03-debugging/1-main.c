5114e809d2b3335a44d7ce8e5ec4f080811b2731
Prince Udeh <arinzechukwup@yahoo.com>
2022-09-13 19:26:22 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * * Return: 0
+ * Return: 0
  */
 
 int main(void)
@@ -14,7 +14,6 @@ int main(void)
 	i = 0;
 	/*
 	 * while (i < 10)
-	 *
 	 * {
 	 * putchar(i);
 	 * }
