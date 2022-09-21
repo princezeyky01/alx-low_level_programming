e531e66a27fa027149ae87b0ccea675a326028aa
Prince Udeh <arinzechukwup@yahoo.com>
2022-09-21 16:37:08 -0700

    A function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,25 +1,42 @@
 #include "main.h"
 
 /**
-* print_number - prints an integer.
-* @n: integer
-* Return: void
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: returns the encoded string
 */
 
-void print_number(int n)
+char *rot13(char *str)
 {
-	int divisor = 1, i, resp;
-
-	if (n < 0)
+	int index1, index2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+		'G', 'H', 'I', 'J', 'K', 'L',
+		'M', 'N', 'O', 'P', 'Q', 'R',
+		'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E',
+		'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'n', 'o', 'p', 'q',
+		'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'a', 'b', 'c',
+		'd', 'e', 'f', 'g', 'h', 'i',
+		'j', 'k', 'l', 'm'};
+	while (str[++index1])
 	{
-		_putchar('-');
-		n *= -1;
+		for (index2 = 0; index2 < 52; index2++)
+		{
+			if (str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2]
+					break;
+			}
+		}
 	}
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-	}
+	return (str);
 }
