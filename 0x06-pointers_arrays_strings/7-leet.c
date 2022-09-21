6d942531aae55f5ab258815038f37dd96b847415
Prince Udeh <arinzechukwup@yahoo.com>
2022-09-21 16:02:15 -0700

    A function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,28 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* leet - function that encode a string
-*@str:string that will be encoded
-*Return:returns encoded string
+* leet - transform to leet
+* @s: char array string type
+* Return: s transformed
 */
 
-char *leet(char *str)
+char *leet(char *s)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, ii;
+	char s1[] = "aeotl";
+	char S1[] = "AEOTL";
+	char s2[] = "43071";
 
-	while (str[++index1])
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (ii = 0; ii < 5; ii++)
 		{
-			if (str[index1] == leet[index2] ||
-			str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (s[i] == s1[ii] || s[i] == S1[ii])
+			{
+				s[i] = s2[ii];
+				break;
+			}
 		}
 	}
-	return (str);
+	return (s);
 }
