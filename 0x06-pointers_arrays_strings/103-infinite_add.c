9cda61eaaf1fb2d9a03a62432449d6c082337e0c
Prince Udeh <arinzechukwup@yahoo.com>
2022-09-21 19:55:45 -0700

    A function that adds two number

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,57 +1,48 @@
 #include "main.h"
 
 /**
-* infinite_add - add 2 integers.
-* @n1: integer
-* @n2: integer
-* @r: buffer
-* size_r: size of r
-* Return: char
+* infinite_add - adds two numbers
+* @n1: first number
+* @n2: second number
+* @r: buffer for result
+* @size_r: buffer size
+*
+* Return: address of r or 0
 */
-
-int _atoi(char *s)
-{
-	int sign = 1, resp = 0, firstNum;
-
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-	return (sign * resp);
-}
-
-void int_to_string(int n)
-{
-	int divisor = 1, i, resp;
-
-	for (i = 0; n / divisor > 9; i++)
-	{
-		divisor *= 10;
-	}
-
-	char str[i];
-
-	for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
-	{
-		resp = n / divisor;
-		str[cmpt] = '0' + resp;
-		n = n - resp * divisor;
-	}
-	str[i] = ('0' + n);
-}
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int sum, a, b;
-	a = _atoi(n1);
-	b = _atoi(n2);
-	sum = a + b;
+	int i, j, k, l, m, n;
+
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
